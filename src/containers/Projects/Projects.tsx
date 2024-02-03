8efaa3a4f0f6ddec74574b20fd191579282a8898
Denis K <wolf-den@tut.by>
2024-02-03 13:51:25 +0300

    feat: add swiper for projects

--- src/containers/Projects/Projects.tsx
+++ src/containers/Projects/Projects.tsx
@@ -1,14 +1,11 @@
-import projectsData from '../../utils/projects-data';
-import Project from '../Project/Project';
+import SwiperProjects from '../SwiperProjects/SwiperProjects';
 import './Projects.scss';
 
 const Projects = () => (
   <div className="projects">
     <div className="projects__title">Projects</div>
     <div className="projects__divider" />
-    {projectsData.map(({ photoAlign, title, description, linkUrl, srcImage }) => (
-      <Project photoAlign={photoAlign} title={title} description={description} linkUrl={linkUrl} srcImage={srcImage} />
-    ))}
+    <SwiperProjects />
   </div>
 );
 
--- src/utils/projects-data.ts
+++ src/utils/projects-data.ts
@@ -7,6 +7,22 @@ const projectsData = [
     linkUrl: 'https://alitools.io/',
     srcImage: './projects/745606896.jpg',
   },
+  {
+    photoAlign: 'left',
+    title: 'Impact Factors',
+    description: 'Creating a widget for the site-builder and working with admin CRM',
+    technologies: 'sssssssssss',
+    linkUrl: 'https://impactfactors.net/',
+    srcImage: './projects/photo_2022-09-21_13-44-51.jpg',
+  },
+  {
+    photoAlign: 'right',
+    title: 'Musio',
+    description: 'Creating a page of costs and schedules for this',
+    technologies: 'sssssssssss',
+    linkUrl: 'https://crm.musio.io/',
+    srcImage: './projects/p-3.jpg',
+  },
 ];
 
 export default projectsData;
