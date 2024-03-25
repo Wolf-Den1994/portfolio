dd670d9949a0f3d0f12734d53198de0271abf8f3
Denis K <wolf-den@tut.by>
2024-03-25 19:47:39 +0300

    feat: translate footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,20 @@
+import { useTranslation } from 'react-i18next';
 import LavaLamp from '../LavaLamp/LavaLamp';
 import './Footer.scss';
 
-const Footer = () => (
-  <footer className="footer">
-    <div className="footer-priority wow fadeInUp">
-      Prior to collaboration, it&#39;s essential to acknowledge that, as a seasoned web developer, I&#39;m unable to
-      accommodate &#34;urgent tasks&#34; or engage in projects of a duration shorter than a day. My focus is on
-      cultivating enduring partnerships rather than pursuing quick, short-term gains.
-    </div>
-    <div className="footer-copy wow fadeInUp">Denis Karazan 2024</div>
-    <div className="footer-space" />
-    <div className="footer-lava-lamp">
-      <LavaLamp />
-    </div>
-  </footer>
-);
+const Footer = () => {
+  const { t } = useTranslation();
+
+  return (
+    <footer className="footer">
+      <div className="footer-priority wow fadeInUp">{t('footer.priority')}</div>
+      <div className="footer-copy wow fadeInUp">{t('logo.name')}, 2024</div>
+      <div className="footer-space" />
+      <div className="footer-lava-lamp">
+        <LavaLamp />
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
