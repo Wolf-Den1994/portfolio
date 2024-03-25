ada965e14b35fc395c439c45c411b06464592928
Denis K <wolf-den@tut.by>
2024-03-25 19:07:37 +0300

    feat: translate education

--- src/containers/Education/Education.tsx
+++ src/containers/Education/Education.tsx
@@ -1,24 +1,27 @@
+import { useTranslation } from 'react-i18next';
 import SlideToggleWrap from '../../components/SlideToggleWrap/SlideToggleWrap';
 import Title from '../../components/Title/Title';
 import certificateData from '../../data/certificates';
 import './Education.scss';
 
 const Education = () => {
+  const { t } = useTranslation();
+
   const handleClick = (link: string) => {
     window.open(link);
   };
 
   return (
     <div className="education">
-      <Title text="Education" />
+      <Title text={t('education.title')} />
       <div className="certificates">
         {certificateData.map(({ sertName, course, date, moreInfo, blockAlignLeft, isMoreInfoImage, linkSert }) => (
           <SlideToggleWrap
             key={date}
-            title={sertName}
-            description={course}
-            date={date}
-            moreInfo={moreInfo}
+            title={t(sertName)}
+            description={t(course)}
+            date={t(date)}
+            moreInfo={isMoreInfoImage ? moreInfo : t(moreInfo)}
             isMoreInfoImage={isMoreInfoImage}
             blockAlignLeft={blockAlignLeft}
             onClick={() => handleClick(linkSert)}
